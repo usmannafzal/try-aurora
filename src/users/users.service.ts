8d6f7778ea05b3da6b4c11e30e2dd1c3d340a056
Usman Afzal <109211422+usmannafzal@users.noreply.github.com>
2025-02-28 02:24:38 +0500

    User created method refactored

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,17 +9,16 @@ export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   async create(body: CreateUserDto, otp: number) {
+    const otpExpiresAt = new Date(Date.now() + 5 * 60 * 1000); // Expiry time in 5 minutes
+
     let user = await this.repo.findOne({ where: { email: body.email } });
-    if (user) {
-      user.otp = otp;
-      user.otpExpiresAt = new Date(new Date().getTime() + 5 * 60 * 1000);
-      return await this.repo.save(user);
+
+    if (!user) {
+      user = this.repo.create({ email: body.email, otp, otpExpiresAt });
+    } else {
+      Object.assign(user, { otp, otpExpiresAt });
     }
 
-    user = new User();
-    user.email = body.email;
-    user.otp = otp;
-    user.otpExpiresAt = new Date(new Date().getTime() + 5 * 60 * 1000);
     return this.repo.save(user);
   }
 }
