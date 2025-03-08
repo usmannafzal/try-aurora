03040e512fe6ece4471abe8a7fbfc6b3a060a03c
Usman Afzal <109211422+usmannafzal@users.noreply.github.com>
2025-03-08 05:25:40 +0500

    FormData accessed

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ProjectsService } from './projects.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('projects')
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  create(@UploadedFile() file: Express.Multer.File, @Body() body: any) {
+    console.log(body);
+  }
 }
